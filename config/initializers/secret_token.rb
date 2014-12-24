e57aa29c7034275429238ff71e2b7b32012b8763
Noah Davis <noahd1@yahoo.com>
2014-12-24 13:54:31 -0500

    Result of Can't initialize a new Rails application within the directory of another, please change to a non-Rails directory first. Type 'rails' for help.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimplyVulnerable::Application.config.secret_token = '2c52156a048cf209770831eb2e0629eedb73b8319c344156f684e529960ac594c900d5b6e2369e5aeaa643a3325953589ec435511106b42112b3af3edff208cf'
